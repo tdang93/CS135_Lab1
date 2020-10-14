ca5f38708ef47ceda5cf7e8662b4ccfedf441b93
Dylan Han <dhan013@ucr.edu>
2020-10-13 19:37:04 -0700

    added light script

---
+++ Assets/Scripts/Lightswitch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightswitch : MonoBehaviour{
+    // Start is called before the first frame update
+    int counter = 0;
+    void Start(){
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (Input.GetKeyDown("tab")){
+            counter++;
+            if(counter == 1)
+            {
+                light.color = red;
+            }
+            else if (counter == 2)
+            {
+                light.color = blue;
+            }
+            else if (counter == 3)
+            {
+                light.color = green;
+                counter = 0;
+            }
+            else
+            {
+                light.color = purple;
+            }
+        }
+    }
+}
