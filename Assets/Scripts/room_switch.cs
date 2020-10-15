10759d3e68b48e87612b2a09d1b076a557fba46a
Dylan Han <dhan013@ucr.edu>
2020-10-14 23:22:54 -0700

    Added teleportation in room_switch.cs

---
+++ Assets/Scripts/room_switch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class room_switch : MonoBehaviour
+{
+    private Transform t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (Input.GetKeyDown("2"))
+        {
+            transform.position = new Vector3(6, 11, 21);
+        }
+    }
+
+}
+
