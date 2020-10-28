2d7950ad2ee9c6947e363e8d808fa99ab9e2ce1d
Dylan Han <dhan013@ucr.edu>
2020-10-27 18:14:39 -0700

    backup before api update

---
+++ Assets/Scripts/BallDropScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDropScript : MonoBehaviour
+{
+    public GameObject ball;
+    void OnTriggerEnter(Collider other)
+    {
+        ball.GetComponent<Rigidbody>();
+        ball.rigidbody.useGravity = true;
+    }
+
+}
+
